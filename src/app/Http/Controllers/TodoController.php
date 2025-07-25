4b9442af7a8c1245be441514e971cffa3f223aaa
米本 朋未 <yonemototomomi@yonemotonoMacBook-Air.local>
2025-07-25 17:42:41 +0900

    feat：DBから取得したデータを画面に渡す処理を実装(view関数を使用しcontrollerからHTMLを表すbladeファイルへデータを移行)

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -10,10 +10,10 @@ class TodoController extends Controller
 {
     public function index()
     {
-
         $todo = new Todo();
         $todos = $todo->all();
 
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);
     }
 }
+
