e311ed3ab70c47b1945798bfb719abdb8ebea801
米本 朋未 <yonemototomomi@yonemotonoMacBook-Air.local>
2025-07-28 19:12:13 +0900

    feat:詳細画面遷移ボタンを一覧画面に追加し、ルートを定義

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('/todo', 'TodoController@index')->name('todo.index');
+
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
