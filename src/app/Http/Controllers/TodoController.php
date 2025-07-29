d39a8e19c06d49d5d7eda9724975bcd800dd087b
米本 朋未 <yonemototomomi@yonemotonoMacBook-Air.local>
2025-07-29 11:56:49 +0900

    feat:リクエストされた値を取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -62,4 +62,9 @@ class TodoController extends Controller
         $todo = $this->todo->find($id);
         return view('todo.edit', ['todo' => $todo]);
     }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+    }
 }
